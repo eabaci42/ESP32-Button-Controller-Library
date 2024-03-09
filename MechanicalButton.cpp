493756270e5960dd60b853a5b205ab051fbb6478
rulert <ertugrul@atikrost.com>
2024-03-09 13:14:38 +0300

    Analog/Digital Readable

--- MechanicalButton.cpp
+++ MechanicalButton.cpp
@@ -1,31 +1,88 @@
-// MechanicalButton.cpp
 #include "MechanicalButton.h"
 
-MechanicalButton::MechanicalButton(int pin, int mode, unsigned long debounceDelay) {
-    _pin = pin;
-    _mode = mode; // Save the mode (0 for pull-up, 1 for pull-down)
-    _debounceDelay = debounceDelay;
-    _pressed = false;
-    _released = false;
-    _holdFlag = false;
-    _lastDebounceTime = 0;
-    _lastPressTime = 0;
-    pinMode(_pin, INPUT); // Always set to INPUT mode
+// Analog okumalar için 1V eşik değeri ESP32 üzerindeki maksimum 3.3V referansına göre hesaplanır
+// 1V eşik değeri için analog değer: 1V / 3.3V * 4095 ≈ 1241
+// Bu değer kurucu fonksiyon üzerinden alınacaktır, dolayısıyla burada tanımlamaya gerek yoktur.
 
-    _state = (_mode == 0) ? HIGH : LOW; // Initialize based on mode
-    _lastState = _state;
+MechanicalButton::MechanicalButton(int pin, char readType, int mode, unsigned long debounceDelay, int threshold) : _pin(pin),
+                                                                                                                   _readType(readType),
+                                                                                                                   _mode(mode),
+                                                                                                                   _debounceDelay(debounceDelay),
+                                                                                                                   _threshold(threshold), // Eşik değeri kurucu fonksiyonda ayarlanır.
+                                                                                                                   _pressed(false),
+                                                                                                                   _released(false),
+                                                                                                                   _holdFlag(false),
+                                                                                                                   _lastDebounceTime(0),
+                                                                                                                   _lastPressTime(0),
+                                                                                                                   _state(false),
+                                                                                                                   _lastState(false)
+{
+    pinMode(_pin, INPUT); // ESP32 için, analog ve dijital pin modunu ayarla
 }
+/*
+void MechanicalButton::update()
+{
+    bool reading = false;
+    if (_readType == 'a')
+    { // Analog okuma modu
+        int analogValue = analogRead(_pin);
+        reading = analogValue > _threshold; // Eşik değeri üzeri okumaları kabul et
+    }
+    else
+    { // Dijital okuma modu
+        reading = digitalRead(_pin) == HIGH;
+    }
+
+    // Debounce mantığı
+    if (reading != _lastState)
+    {
+        _lastDebounceTime = millis();
+    }
+    if ((millis() - _lastDebounceTime) > _debounceDelay)
+    {
+        if (reading != _state)
+        {
+            _state = reading;
+            if (reading)
+            {
+                _pressed = true;
+                _lastPressTime = millis();
+            }
+            else
+            {
+                _released = true;
+                _holdFlag = false;
+            }
+        }
+    }
+    _lastState = reading;
+}
+*/
+
+
 
 void MechanicalButton::update() {
-    bool reading = digitalRead(_pin);
+    bool reading = false;
+    
+    // Analog veya dijital okuma moduna göre okuma yap
+    if (_readType == 'a') {
+        // Analog okuma yap ve son değeri _lastAnalogValue'de sakla
+        _lastAnalogValue = analogRead(_pin);
+        // Eşik değerin üzerindeyse, reading'i true olarak ayarla
+        reading = _lastAnalogValue > _threshold;
+    } else {
+        // Dijital okuma yap
+        reading = digitalRead(_pin) == HIGH;
+    }
+
+    // Debounce mantığı
     if (reading != _lastState) {
         _lastDebounceTime = millis();
     }
     if ((millis() - _lastDebounceTime) > _debounceDelay) {
         if (reading != _state) {
             _state = reading;
-            // Adjust logic based on the mode
-            if ((_mode == 0 && _state == LOW) || (_mode == 1 && _state == HIGH)) {
+            if (reading) {
                 _pressed = true;
                 _lastPressTime = millis();
             } else {
@@ -37,32 +94,48 @@ void MechanicalButton::update() {
     _lastState = reading;
 }
 
-bool MechanicalButton::isPressed() {
-    if (_pressed) {
+bool MechanicalButton::isPressed()
+{
+    if (_pressed)
+    {
         _pressed = false;
         return true;
     }
     return false;
 }
 
-bool MechanicalButton::wasPressed() {
-    // Adjust logic based on the mode
-    return (_mode == 0) ? _state == LOW : _state == HIGH;
+bool MechanicalButton::wasPressed()
+{
+    // Moda bağlı mantık
+    return (_mode == 0) ? !_state : _state;
 }
 
-bool MechanicalButton::wasReleased() {
-    if (_released) {
+bool MechanicalButton::wasReleased()
+{
+    if (_released)
+    {
         _released = false;
         return true;
     }
     return false;
 }
 
-bool MechanicalButton::isHeld(unsigned long holdTime) {
-    bool condition = (_mode == 0) ? _state == LOW : _state == HIGH;
-    if (condition && !_holdFlag && (millis() - _lastPressTime >= holdTime)) {
+bool MechanicalButton::isHeld(unsigned long holdTime)
+{
+    bool condition = (_mode == 0) ? !_state : _state;
+    if (condition && !_holdFlag && (millis() - _lastPressTime >= holdTime))
+    {
         _holdFlag = true;
         return true;
     }
     return false;
+}
+
+
+int MechanicalButton::getAnalogValue() {
+    if (_readType == 'a') {
+        return _lastAnalogValue; // Analog moddaysa, son okunan analog değeri döndür
+    } else {
+        return -1; // Dijital moddaysa, mantıklı bir değer döndürme
+    }
 }
--- MechanicalButton.h
+++ MechanicalButton.h
@@ -1,4 +1,3 @@
-// MechanicalButton.h
 #ifndef MECHANICALBUTTON_H
 #define MECHANICALBUTTON_H
 
@@ -6,15 +5,18 @@
 
 class MechanicalButton {
 public:
-    MechanicalButton(int pin, int mode, unsigned long debounceDelay = 50);
+    // Kurucu fonksiyonu güncellendi: okuma modu ('a' için analog, 'd' için dijital) ve eşik değeri parametreleri eklendi.
+    MechanicalButton(int pin, char readType = 'a', int mode = 1, unsigned long debounceDelay = 50, int threshold = 512); // Analog okuma için varsayılan eşik değeri 512 olarak ayarlandı.
     void update();
     bool isPressed();
     bool wasPressed();
     bool wasReleased();
     bool isHeld(unsigned long holdTime = 1000);
+    int getAnalogValue(); // Son okunan analog değeri döndüren yeni metod eklendi.
 
 private:
     int _pin;
+    char _readType; // 'a' için analog, 'd' için dijital
     int _mode; // 0 for pull-up, 1 for pull-down
     unsigned long _debounceDelay;
     unsigned long _lastDebounceTime;
@@ -24,6 +26,8 @@ private:
     bool _pressed;
     bool _released;
     bool _holdFlag;
+    int _threshold; // Analog okuma için eşik değeri
+    int _lastAnalogValue; // Son okunan analog değeri saklar
 };
 
 #endif
